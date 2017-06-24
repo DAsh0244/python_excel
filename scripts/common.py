158ffd166a77bf5be66c21d469d060b7b5edc78c
an-oreo <an.oreo@live.com>
2017-06-24 11:50:43 -0500

    fixed single chunk concat

--- scripts/common.py
+++ scripts/common.py
@@ -1,44 +1,108 @@
+#! /usr/bin/env python
+# vim:fileencoding=utf-8
+# -*- coding: utf-8 -*-
+"""
+excel utils
+common.py
+Author: Danyal Ahsanullah
+Date: 2017, June 16
+Copyright (c):  2017 Danyal Ahsanullah
+License: MIT (see LICENSE.txt)
+Description: common functions that will be used between scripts
+"""
+
 import time as _time
 import openpyxl as _openpyxl
 import string as _string
 import itertools as _itertools
+import os as _os
+import logging as _logging
+from openpyxl.utils.cell import *
+
+
+__author__ = 'danyal.ahsanullah@gmail.com'
+__version__ = '0.0.2'
+
+
+_formatter = _logging.Formatter('%(asctime)s - %(message)s')
+# _logger = _logging.getLogger(__file__)
+_logger = _logging.getLogger('A')
+# logger.setLevel(logging.DEBUG)  # defaults to logging.WARNING
+_ch = _logging.StreamHandler()
+_ch.setLevel(_logging.DEBUG)
+_ch.setFormatter(_formatter)
+_logger.addHandler(_ch)
+_logger.setLevel(_logging.DEBUG)
+
+
+def ensure_dir(file_path):
+    directory = _os.path.dirname(file_path)
+    if not _os.path.exists(directory):
+        _os.makedirs(directory)
+
+
+def path_leaf(path):
+    head, tail = _os.path.split(path)
+    return tail or _os.path.basename(head)
+
+
+def sorted_files(files, order):
+    # key_func = lambda x: order.index(path_leaf(x))
+    def key_func(x):
+        return order.index(path_leaf(x))
+    if any([path_leaf(entry) not in order for entry in files]):
+        raise ValueError('file(s) given not listed in order.')
+    if any([entry not in [path_leaf(file) for file in files] for entry in order]):
+        raise ValueError('file(s) in order missing in provided files.')
+    return sorted(files, key=key_func)
 
 
 def pairwise(iterable):
-    "s -> (s0,s1), (s1,s2), (s2, s3), ..."
+    """"s -> (s0,s1), (s1,s2), (s2, s3), ..."""
     a, b = _itertools.tee(iterable)
     next(b, None)
     return zip(a, b)
 
-def grouped(iterable, n):
-    "s -> (s0,s1,s2,...sn-1), (sn,sn+1,sn+2,...s2n-1), (s2n,s2n+1,s2n+2,...s3n-1), ..."
-    return zip(*[iter(iterable)]*n)
 
-def filter_list(full_list, excludes):
+def grouped(iterable, n):
+    """s -> (s0,s1,s2,...sn-1), (sn,sn+1,sn+2,...s2n-1), (s2n,s2n+1,s2n+2,...s3n-1), ..."""
+    return zip(*[iter(iterable)] * n)
+
+
+def filter_list(full_list, excludes=(None, '')):
     s = set(excludes)
     return (x for x in full_list if x not in s)
-    
+
+
 def flatten(iterable, n=1):
     """
+    remove n levels of list nesting for a list of lists
     assumes uniform nesting
+    :param iterable: nested list of lists to be flattened
+    :param n: how many levels of nesting to remove
+    :return: flattened list
     """
     tmp = iterable
-    for i in range(n):
+    for _ in range(n):
         tmp = [item for sublist in tmp for item in sublist]
     return tmp
-    
+
+
 def _sec2time(sec, n_msec=5):
-    ''' 
-    Convert seconds to "D days, HH:MM:SS.FFF"    
-    '''
-    if hasattr(sec,'__len__'):
+    """
+    Convert seconds to "D days, HH:MM:SS.FFF"
+    :param sec: number of seconds
+    :param n_msec: number of milliseconds to use
+    :return: string representing of time in human readable format
+    """
+    if hasattr(sec, '__len__'):
         return [_sec2time(s) for s in sec]
     m, s = divmod(sec, 60)
     h, m = divmod(m, 60)
     d, h = divmod(h, 24)
-    d, h, m = int(d),int(h), int(m) 
+    d, h, m = int(d), int(h), int(m)
     if n_msec > 0:
-        pattern = '{{:02d}}:{{:02d}}:{{:0{space}.{msec}f}}'.format(space=n_msec+3, msec=n_msec)
+        pattern = '{{:02d}}:{{:02d}}:{{:0{space}.{msec}f}}'.format(space=n_msec + 3, msec=n_msec)
         # pattern = '%%02d:%%02d:%%0%d.%df' %(n_msec+3, n_msec)
     else:
         pattern = '{:02d}:{:02d}:{:02d}'
@@ -48,32 +112,62 @@ def _sec2time(sec, n_msec=5):
         return pattern.format(h, m, s)
     return ('{:02d} days, ' + pattern).format(d, h, m, s)
 
+
 def time_execution(desc=None, alert=None):
+    """
+    decorator to measure function execution time
+    :param desc: string to be used inplace of function name
+    :param alert: string to be displayed before function call
+    :return: N/A
+    """
     def decorator(method):
         def wrapper(*args, **kw):
             if alert:
+                # _logger.info('\n{}:'.format(alert.title()))
                 print('\n{}:'.format(alert.title()))
             t1 = _time.perf_counter()
             result = method(*args, **kw)
             t2 = _time.perf_counter()
+            time_elapsed = _sec2time(t2 - t1)
             if desc:
-                msg = '{} took {}'.format(desc, _sec2time(t2 - t1))
+                msg = '{} took {}'.format(desc, time_elapsed)
             else:
-                msg = '"{}": Operation took {}'.format(method.__name__, _sec2time(t2 - t1))
-            print(msg)
+                msg = '"{}": Operation took {}'.format(method.__name__,  time_elapsed)
+            _logger.info(msg)
             return result
         return wrapper
     return decorator
 
+
 def write_row(row_num, row_data, sheet):
-    for col,cell in enumerate(row_data, start=1):
+    """
+    writes a row of data to the specified row num
+    :param row_num: row num to write row data to
+    :param row_data: data to be written to the row
+    :param sheet: sheet to write row to
+    :return: N/A
+    """
+    for col, cell in enumerate(row_data, start=1):
         sheet.cell(row=row_num, column=col).value = cell.value
-    
+
+
 def excel_mappings(title_row):
+    """
+    creates a dictionary that shows mappings between cell contents and cell columns
+    :param title_row: list of openpyxl.cell.Cell
+    :return: dict of col names to col vals
+    """
     return {entry.value.strip(): entry.column for entry in title_row}
-    
+
+
 def inv_map(dictionary: dict):
+    """
+    creates a inverse mapped dictionary of the provided dict
+    :param dictionary: dictionary to be reverse mapped
+    :return: inverse mapped dict
+    """
     return {v: k for k, v in dictionary.items()}
+
 
 def increment_char(c):
     """
@@ -81,33 +175,61 @@ def increment_char(c):
     """
     return chr(ord(c) + 1) if c != 'Z' else 'A'
 
+
 def increment_str(s):
     lpart = s.rstrip('Z')
     num_replacements = len(s) - len(lpart)
     new_s = lpart[:-1] + increment_char(lpart[-1]) if lpart else 'A'
     new_s += 'A' * num_replacements
     return new_s
-    
+
+
 def col2num(col):
     num = 0
     for c in col:
         if c in _string.ascii_letters:
             num = num * 26 + (ord(c.upper()) - ord('A')) + 1
-    return num    
+    return num
+
 
 def colnum_string(n):
-    div=n
-    string=""
-    temp=0
-    while div>0:
-        module=(div-1)%26
-        string=chr(65+module)+string
-        div=int((div-module)/26)
+    div = n
+    string = ""
+    while div > 0:
+        mod = (div - 1) % 26
+        string = chr(65 + mod) + string
+        div = int((div - mod) / 26)
     return string
-    
-def row_vals(row):
-    return [cell.value for cell in row]
-    
+
+
+def row_vals(row, gen=False):
+    """
+    return a list of values for a list of openpyxl.cell.Cell
+    :param row: list of openpyxl.cell.Cell
+    :param gen: if true, returns generator object
+    :return: N/A
+    """
+    if gen:
+        return (cell.value for cell in row)
+    else:
+        return [cell.value for cell in row]
+
+
+def get_cols_by_name(start, stop, inclusive=True):
+    if inclusive:
+        cols = list(map(colnum_string, range(col2num(start), col2num(stop) + 1)))
+    else:
+        cols = list(map(colnum_string, range(col2num(start), col2num(stop))))
+    return cols
+
+
+def format_title_row(row, cols, tag='Interest', tag_label=':'):
+    leading_tag = '{}{} '.format(tag, tag_label)
+    for cell in row:
+        if cell.column in cols and not cell.value.startswith(leading_tag):
+            cell.value = leading_tag + cell.value
+
+
 @time_execution('getting workbook')
 def get_workbook(args):
     wb = _openpyxl.load_workbook(args.file)
@@ -116,26 +238,24 @@ def get_workbook(args):
     else:
         ws = wb.get_sheet_by_name(args.sheet)
     return wb, ws
-    
+
+
 @time_execution('writing workbook')
 def write_workbook(wb, args):
     if not args.output:
         wb.save(args.file)
     else:
         wb.save(args.output)
-           
-def get_cols_by_name(start, stop, inclusive=True):
-    if inclusive:
-        cols = list(map(colnum_string, range(col2num(start), col2num(stop)+1)))
-    else:
-        cols = list(map(colnum_string, range(col2num(start), col2num(stop))))
-    return cols
 
-def format_title_row(row, cols, tag='Interest', tag_label=':'):
-    leading_tag = '{}{} '.format(tag, tag_label)
-    for cell in row:
-        if cell.column in cols and not cell.value.startswith(leading_tag):
-            cell.value = leading_tag + cell.value
+
+# @time_execution
+def check_ws_dim(sheet):
+    if sheet.calculate_dimension() == 'A1:A1':
+        _logger.warning('worksheet dimensions incorrect, recalculating...')
+        sheet.max_row = sheet.max_column = None
+        dim = sheet.calculate_dimension(force=True)
+        _logger.warning('recalculated dimensions as {}'.format(dim))
+        _logger.warning('press <Ctrl+c> to cancel if dimensions are incorrect')
 
 
 ''' EOF '''
--- scripts/excel_concat.py
+++ scripts/excel_concat.py
@@ -1,146 +1,217 @@
-'''
-python excel_concat.py JournalExport.xlsx JournalExportCopy-JB.xlsx -H -k -o journal_outputs\test_out.xlsx -c 20000
-'''
+#! /usr/bin/env python
+# vim:fileencoding=utf-8
+# -*- coding: utf-8 -*-
+"""
+excel utils
+excel_concat.py
+Author: Danyal Ahsanullah
+Date: 2017, June 16
+Copyright (c):  2017 Danyal Ahsanullah
+License: MIT (see LICENSE.txt)
+Description: CLI tool used to join/split excel files.
+"""
 
 
+import re
 import os
 import sys
+import glob
+import logging
 import openpyxl
 import argparse
-import re
 import itertools
 
-from common import *
+import common
+from common import time_execution
 
-__version__ = '0.0.1'
+__author__ = 'danyal.ahsanullah@gmail.com'
+__version__ = '0.0.2'
 
-# cli parser definition
-parser = argparse.ArgumentParser()
-parser.add_argument('file', type=str, nargs='+', help='file(s) to be read in.')
-parser.add_argument('--chunk_size', '-c', type=int, default='100', help='How many entries to write to new file(s). A size less than zero will write to a single file')
-parser.add_argument('--output', '-o', type=str, help='base file name to save as. Resulting files will be incremented on the end. If the path given doesnt exist, it will be made.')
-parser.add_argument('--h_concat', '-H', action="store_true", help='Join files in a sytle of horizontal row appending')
-parser.add_argument('--keep_title', '-k', action="store_true", help='Whether to keep title row in each chunked file or not.')
+# logger creation
+formatter = logging.Formatter('%(asctime)s - %(message)s')
+logger = logging.getLogger(__file__)
+# logger.setLevel(logging.WARNING)  # defaults to logging.WARNING
+ch = logging.StreamHandler()
+ch.setLevel(logging.WARNING)
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+# cli parser creation
+parser = argparse.ArgumentParser('excel_concat')
+parser.add_argument('file', type=str, nargs='+', help='file(s) to be read in')
+parser.add_argument('--path', '-p', action="store_true",
+                    help='if enabled, treats <file> arg as set of paths to dirs of files')
+parser.add_argument('--chunk', '-c', type=int, default=1000,
+                    help='How many entries to write to new file(s). A size less than zero will write to a single file')
+parser.add_argument('--output', '-o', type=str,
+                    help='base file name to save as. Resulting files will be incremented on the end.'
+                         'If the path given doesnt exist, it will be made')
+parser.add_argument('--H_concat', '-H', action="store_true", help='Join files in a sytle of horizontal row appending')
+parser.add_argument('--keep_title', '-k', action="store_true",
+                    help='Whether to keep title row in each chunked file or not')
+parser.add_argument('--order', '-O', type=argparse.FileType('r'),
+                    help='Path to file that will dictate file order to be processed in')
 parser.add_argument('--no_overwrite', '-n', action="store_true", help='If set, will not overwrite files')
+loudness = parser.add_mutually_exclusive_group()
+
+loudness.add_argument('--verbose', '-v', action="count", help='prints a more detailed output')
+loudness.add_argument('--quiet', '-q', action="count", help='suppresses console output')
 parser.add_argument('--version', action='version', version=__version__)
+# parser.add_argument('_args', nargs=argparse.REMAINDER)
 # parser.add_argument('file', type=argparse.FileType('r'), nargs='+', help='file(s) name to be read in.')
 # parser.add_argument('--sheet', '-s', type=str, nargs='+', help='sheet name to pull data from')
 
-@time_execution(alert='loading workbooks')
-def load_wbs(params):
+
+@time_execution(alert='loading workbooks', desc='loading workbooks')
+def load_wbs(files, _read_only=True):
     wbs = []
     wss = []
-    for file in params.file:
-        book = openpyxl.load_workbook(file, read_only=True)
+    for file in files:
+        book = openpyxl.load_workbook(file, read_only=_read_only)
         ws = book.worksheets[0] 
-        if ws.calculate_dimension() == 'A1:A1':
-            print('worksheet dimensions incorrect, recalculating...')
-            ws.max_row = ws.max_column = None
-            print('recalcualted dimesions as {}'.format(ws.calculate_dimension(force=True)))
-            print('press <Ctrl+c> to cancel if dimensions are incorrect')
-            # tmp_rows = 0
-            # cols = []
-            # while ws.rows:
-                # cols.append(len(next(ws.rows)))
-                # tmp_rows += 1
-            # cols = max(cols)
-            # ws.max_column = colnum_string(cols)
-            # ws.max_row = tmp_rows
-            # print('{}'.format(rows, cols, colnum_string))
+        common.check_ws_dim(ws)
         wbs.append(book)
-        wss.append(book.worksheets[0])
-        print('loaded {}'.format(file))
+        # wss.append(book.worksheets[0])
+        wss.append(ws)
+        logger.warning('loaded {}\n'.format(file))
     return wbs, wss
     
-@time_execution(alert='writing chunks')
-def write_chunked_wbs(wss, chunk_size, concat=False, output=None, keep_title=True):
+
+@time_execution(alert='writing chunks', desc='wrote workbooks')
+def write_chunked_wbs(wss, chunk, concat=False, output=None, keep_title=True):
     chunk_num = 0
     if output is None:
         name = 'chunked_workbook'
         ext = 'xlsx'
     else:
         name, ext = output.split('.')
-    output = '{}_{}.{}'.format(name, chunk_num, ext)
+    output = '{}_{:02d}.{}'.format(name, chunk_num, ext)
     if concat:
-        # print('concat')
-        # for wb1, wb2 in grouped(wbs, 2):
-        for ws1, ws2 in pairwise(wss):
-            # new_wb = openpyxl.Workbook(write_only = True)
-            # print(new_wb)
-            # new_ws = new_wb.create_sheet()
+        for ws1, ws2 in common.pairwise(wss):
+            new_wb = openpyxl.Workbook(write_only=True)
             try:
-                rows1, rows2 =  ws1.rows, ws2.rows 
+                rows1, rows2 = ws1.rows, ws2.rows
                 if keep_title:
-                    title_1 = row_vals(next(rows1))
-                    title_2 = row_vals(next(rows2))[2:]  # first two cols are duplicate of first two cols of first file
+                    title_1 = common.row_vals(next(rows1))
+                    title_2 = common.row_vals(next(rows2))[2:]  # first two cols are duplicate of cols of first
                     # title_2 = list(filter_list(row_vals(next(rows2)), title_1))
-                    title_row = flatten([title_1, title_2])
+                    title_row = common.flatten([title_1, title_2])
                     # print(title_row)
                 while rows1 or rows2:
                     new_wb = openpyxl.Workbook(write_only=True)
                     new_ws = new_wb.create_sheet()
                     if keep_title:
                         new_ws.append(title_row)
-                    if chunk_size < 0:
+                    # try:
+                    #     new_ws.append(title_row)
+                    # except NameError:
+                    #     pass
+                    if chunk < 0:
                         while True:
-                            new_ws.append(flatten([row_vals(next(rows1)), row_vals(next(rows2)[2:])]))  # first two cols are duplicate of first two cols of first file
+                            # first two cols are duplicate of first two cols of first file
+                            new_ws.append(common.flatten([common.row_vals(next(rows1)),
+                                                          common.row_vals(next(rows2)[2:])]))
                             # new_ws.append(flatten([row_vals(next(rows1)), row_vals(next(rows2))]))
                     else:
-                        for line in range(chunk_size):
-                            new_ws.append(flatten([row_vals(next(rows1)), row_vals(next(rows2)[2:])]))  # first two cols are duplicate of first two cols of first file
+                        for line in range(chunk):
+                            # first two cols are duplicate of first two cols of first file
+                            new_ws.append(common.flatten([common.row_vals(next(rows1)),
+                                                          common.row_vals(next(rows2)[2:])]))
                             # new_ws.append(flatten([row_vals(next(rows1)), row_vals(next(rows2))]))
                     new_wb.save(output)
-                    print('wrote {}'.format(output))
+                    logger.debug('wrote {}'.format(output))
                     chunk_num += 1
-                    output = '{}_{}.{}'.format(name, chunk_num, ext)
+                    output = '{}_{:02d}.{}'.format(name, chunk_num, ext)
             except StopIteration:
-                new_wb.save(output)    
-    else: 
-        for ws in wss:
-            # new_wb = openpyxl.Workbook(write_only=True)
-            # new_ws = new_wb.create_sheet()
-            try:
-                rows1 =  ws1.rows 
-                if keep_title:
-                    title_row = row_vals(next(rows1))
-                    print(title_row)
-                while rows1:
-                    new_wb = openpyxl.Workbook(write_only=True)
-                    new_ws = new_wb.create_sheet()
+                new_wb.save(output)
+                logger.debug('wrote {}'.format(output))
+                chunk_num += 1
+                output = '{}_{:02d}.{}'.format(name, chunk_num, ext)
+    else:
+        if chunk < 0:
+            new_wb = openpyxl.Workbook(write_only=True)
+            new_ws = new_wb.create_sheet()
+            title_row = common.row_vals(next(wss[0].rows))
+            new_ws.append(title_row)
+            for ws in wss:
+                for row in ws.rows:
+                    tmp = common.row_vals(row)
+                    if tmp == title_row:
+                        # next(ws.rows)
+                        tmp = common.row_vals(next(ws.rows))
+                    new_ws.append(tmp)
+            new_wb.save(output)
+            logger.debug('wrote {}'.format(output))
+        else:
+            for ws in wss:
+                new_wb = openpyxl.Workbook(write_only=True)
+                try:
+                    rows1 = ws.rows
                     if keep_title:
-                        new_ws.append(title_row)
-                    if chunk_size < 0:
-                        while True:
-                            new_ws.append(row_vals(next(rows1)))
-                    else:
-                        for line in range(chunk_size):
-                            new_ws.append(row_vals(next(rows1)))
+                        title_row = common.row_vals(next(rows1))
+                        # print(title_row)
+                    while rows1:
+                        new_wb = openpyxl.Workbook(write_only=True)
+                        new_ws = new_wb.create_sheet()
+                        try:
+                            new_ws.append(title_row)
+                        except NameError:
+                            pass
+                        for line in range(chunk):
+                            new_ws.append(common.row_vals(next(rows1)))
+                        new_wb.save(output)
+                        logger.debug('wrote {}'.format(output))
+                        chunk_num += 1
+                        output = '{}_{:02d}.{}'.format(name, chunk_num, ext)
+                except StopIteration:
                     new_wb.save(output)
-                    print('wrote {}'.format(output))
+                    logger.debug('wrote {}'.format(output))
                     chunk_num += 1
-                    output = '{}_{}.{}'.format(name, chunk_num, ext)
-            except StopIteration:
-                new_wb.save(output)    
+                    output = '{}_{:02d}.{}'.format(name, chunk_num, ext)
 
-def ensure_dir(file_path):
-    directory = os.path.dirname(file_path)
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-                
-                
 @time_execution('Total operation')
 def main():
     args = parser.parse_args()
-    # print(vars(args))
+
+    if args.verbose:
+        logger.setLevel(logging.DEBUG)
+        ch.setLevel(logging.DEBUG)
+        logger.warning('Operating in verbose mode:')
+    elif args.quiet:
+        logger.setLevel(logging.NOTSET)
+        ch.setLevel(logging.NOTSET)
+    logger.debug(vars(args))
+
+    if args.path:
+        files = []
+        for entry in args.file:
+            files.append(glob.glob(entry))
+        args.file = list(common.filter_list(common.flatten(files)))
+        logger.debug('The following files were found: {}'.format(args.file))
+
+    if args.order:
+        try: 
+            order = args.order.read().splitlines()
+            args.file = common.sorted_files(args.file, order)
+            logger.debug('files successfully reordered')
+        except IOError:
+            raise IOError('File:{} could not be read'.format(args.output.name))
+
     for file in iter(args.file):
         if not os.path.exists(file):
-            raise OSError
+            raise OSError('file not found: {}'.format(file))
+    logger.debug('files were all found')
+
     if args.output:
         os.makedirs(os.path.dirname(args.output), exist_ok=True)
-    workbooks, worksheets = load_wbs(args)
-    write_chunked_wbs(worksheets, args.chunk_size, concat=args.h_concat, 
+        logger.debug('Output path set')
+
+    workbooks, worksheets = load_wbs(args.file)
+
+    write_chunked_wbs(worksheets, args.chunk, concat=args.H_concat, 
                       output=args.output, keep_title=args.keep_title)
-    
+
+                      
 if __name__ == '__main__':
     main()
     sys.exit(0)
